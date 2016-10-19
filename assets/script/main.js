caf902f9b8ed6eaa89f3f58ae19082a61a591f56
Robin Bäcklund <robin.backlund@clickup.se>
2016-10-19 11:33:16 +0200

    Built and designed the landing page
    Designed a new landing page, responsive with images.

---
+++ assets/script/main.js
@@ -0,0 +1,10 @@
+setInterval(function(){
+	$("#scroll-icon").animate({
+	    opacity: 0.2
+	  }, 500);
+}, 1000);
+setInterval(function(){
+	$("#scroll-icon").animate({
+	    opacity: 0.7
+	  }, 500);
+}, 500);
